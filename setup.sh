b05f0a6c2e2c58509c3a1a0b182799dacb0f5113
Gaetan Semet <gaetan@xeberon.net>
2013-11-03 21:17:52 +0100

    create version for xfce
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p ~/.config/autostart/
 cp -v resources/configure-autolock.desktop /usr/share/applications/autolock.desktop
 cp -v resources/autolock.desktop ~/.config/autostart/autolock.desktop
 
